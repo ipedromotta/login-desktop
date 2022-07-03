0a00c03ef3c7e53458054b75ce894579639ab0b5
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2022-07-03 18:43:06 -0300

    :sparkles: feat: Novo recurso de criptografia adicionado

---
+++ Controller/CriptografiaController.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet
+
+
+class CriptografiaController:
+    def __init__(self):
+        self.__chave = b'8g_HV2iOzb0FZjDo4a9iLfj4WkWm4m3pv797GRplmqE=' # Calma, isto é apenas um teste.
+        self.__fernet = Fernet(self.__chave)
+
+    def criptografar(self, senha):
+        try:
+            senha_encriptografada = self.__fernet.encrypt(senha.encode())
+            return senha_encriptografada.decode()
+        except:
+            return False
+
+    def descriptografar(self, cripto, senha):
+        try:
+            senha_descriptografada = self.__fernet.decrypt(cripto.encode()).decode()
+            return senha_descriptografada == senha
+        except:
+            return False
