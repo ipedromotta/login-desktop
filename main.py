956b42b6cecd46e139d67fac2be4d85091642950
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2023-01-01 15:02:44 -0300

    :sparkles: feat: Adicionando tratamento de erro

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-import sys
+import sys, os, signal
 from PyQt5 import QtWidgets
-from View.InterfaceLogin import InterfaceLogin
+
+from Controller.PageController import PageController
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    try:
+        app = QtWidgets.QApplication(sys.argv)
 
-    interface_login = InterfaceLogin()
-    interface_login.show()
+        controller = PageController()
+        controller.mostrar_tela_login()
 
-    sys.exit(app.exec_())
+        if not app.exec_():
+            try:
+                os.kill(os.getpid(), signal.SIGINT)
+                sys.exit(0)
+            except Exception as ex:
+                print(ex)
+                
+    except Exception as ex:
+        app.closeAllWindows()
+        app.quit()
+        os.kill(os.getpid(), signal.SIGINT)
+        print(ex)
+        sys.exit(0)
