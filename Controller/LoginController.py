fe2fcbe610db54c3630dc5d9abdccd5199f6746f
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2023-01-01 15:02:21 -0300

    :recycle: refactor: Melhorando estrutura login controller

--- Controller/LoginController.py
+++ Controller/LoginController.py
@@ -1,19 +1,47 @@
+from PyQt5 import QtWidgets, QtCore
+
 from Model.UsuarioModel import UsuarioModel
 
+from View.Login import Ui_Login
+from View.Logado import Ui_Logado
 
-class LoginController:
-    def login(self, interface, usuario:str, senha:str, conn) -> None:
+
+class LoginController(QtWidgets.QMainWindow):
+    tela_cadastro = QtCore.pyqtSignal()
+    tela_administrador = QtCore.pyqtSignal()
+      
+    def __init__(self, conn=None) -> None:
+        super().__init__()
+        self.ui = Ui_Login()
+        self.iniciar_eventos()
+        self._conn = conn
+        
+    def iniciar_eventos(self):
+        self.ui.setupUi(self)
+        self.ui.btn_entrar.clicked.connect(lambda: self.logar(self.ui.usuario.text(), self.ui.senha.text(), self._conn))
+        self.ui.btn_cadastrar.clicked.connect(self.tela_cadastro.emit)
+    
+    def logar(self, usuario:str, senha:str, conn) -> None:
         if usuario == "" or senha == "":
-            interface.msg.emit("Preencha todos os campos!")
+            self.ui.lbl_erro.setText("Preencha todos os campos!")
             return
 
         usuario = UsuarioModel().logar(usuario, senha, conn)
 
         if not usuario:
-            interface.msg.emit("Login ou senha incorreto!")
+            self.ui.lbl_erro.setText("Login ou senha incorreto!")
             return
 
         if usuario.Administrador:
-            interface.admin()
+            self.tela_administrador.emit()
         else:
-            interface.logar(usuario)
+            self.mostrar_tela_logado(usuario)
+
+    def mostrar_tela_logado(self, usuario:object) -> None:
+        try:
+            self.ui_logado = Ui_Logado()
+            self.ui_logado.setupUi(self)
+            self.ui_logado.lbl_nome.setText(f"Olá, {usuario.Nome}!")
+            self.ui_logado.btn_logout.clicked.connect(self.iniciar_eventos)
+        except Exception as ex:
+            print(ex)
